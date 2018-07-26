449ae4e04d12092d7de5605b8d8c43028c96c685
Ronnye Herminio <rpherminio@hotmail.com>
2018-07-25 21:29:10 -0300

    Buscando endereco do banco de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -11,48 +13,23 @@ export class PickAddressPage {
 
   items : EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public storage : StorageService, public clienteService : ClienteService) {
   }
 
   ionViewDidLoad() {
-
-    this.items = [
-      {
-        id : "1",
-        logradouro : "Rua teste 1",
-        numero : "123",
-        complemento : "",
-        bairro : "Centro",
-        cep : "1234567",
-        cidade:{
-          id : "1",
-          nome:"Uberlandia",
-          estado:{
-            id:"1",
-            nome:"Minas Gerais"
-          }
-        }
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email).subscribe(response => {
+        this.items = response['enderecos'];
+        //buscar imagem
       },
-      {
-        id : "2",
-        logradouro : "Rua teste 2",
-        numero : "3",
-        complemento : "",
-        bairro : "Candeias",
-        cep : "45678932",
-        cidade:{
-          id : "2",
-          nome:"Campinas",
-          estado:{
-            id:"2",
-            nome:"São Paulo"
-          }
-        }
-      }
-
-    ]
-
-    console.log('ionViewDidLoad PickAddressPage');
+      error =>{
+        this.navCtrl.setRoot('HomePage');
+      });
+    } else{
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
 }
